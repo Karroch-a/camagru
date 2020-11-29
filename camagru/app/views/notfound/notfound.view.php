9a358a55b37f4dab9bec9d16c9dd28ce11ab7d65
Abdellah Azeroual <aazeroua@e2r4p15.1337.ma>
2020-11-29 17:40:29 +0100

    add page not found

--- camagru/app/views/notfound/notfound.view.php
+++ camagru/app/views/notfound/notfound.view.php
@@ -1,1 +1,13 @@
-<h1>not found </h1>
+<html>
+<head>
+</head>
+    <title>404 NOT FOUND</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../public/css/login.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+<body>
+    <div class="container1">
+            <a href="/index"><img src = "../../public/img/error.png"></a>
+    </div>
+</body>
+</html>
